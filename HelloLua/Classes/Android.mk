16adca7f89c2173fa1ce0eee9fe857f077f7d1d8
folecr <folecr@gmail.com>
2012-02-16 16:35:38 -0800

    HelloLua build * Android makefiles using Android modules * Uses png, jpeg libxml2 as static libraries * Uses cocosdenshion and lua as shared libs

--- HelloLua/Classes/Android.mk
+++ HelloLua/Classes/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := game_logic_static
+LOCAL_MODULE := cocos_game_logic_static
 
 LOCAL_MODULE_FILENAME := libgame_logic
 
@@ -13,16 +13,15 @@ AppDelegate.cpp \
 ../../lua/cocos2dx_support/LuaCocos2d.cpp \
 ../../lua/cocos2dx_support/tolua_fix.c
 
+LOCAL_C_INCLUDES := $(LOCAL_PATH) \
+                    $(LOCAL_PATH)/../../cocos2dx \
+                    $(LOCAL_PATH)/../../cocos2dx/include \
+                    $(LOCAL_PATH)/../../cocos2dx/platform \
+                    $(LOCAL_PATH)/../../CocosDenshion/include \
+                    $(LOCAL_PATH)/../../lua/lua \
+                    $(LOCAL_PATH)/../../lua/cocos2dx_support \
+                    $(LOCAL_PATH)/../../lua/tolua
+
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
 
-LOCAL_STATIC_LIBRARIES += xml2_static_prebuilt
-LOCAL_STATIC_LIBRARIES += jpeg_static_prebuilt
-LOCAL_WHOLE_STATIC_LIBRARIES += cocos2dx_static
-
-LOCAL_SHARED_LIBRARIES := cocosdenshion_shared lua_shared
-            
 include $(BUILD_STATIC_LIBRARY)
-
-$(call import-module,cocos2dx/platform/third_party/android/modules/libpng)
-$(call import-module,cocos2dx/platform/third_party/android/modules/libxml2)
-$(call import-module,cocos2dx/platform/third_party/android/modules/libjpeg)
--- HelloLua/android/jni/Android.mk
+++ HelloLua/android/jni/Android.mk
@@ -1,11 +1,28 @@
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 
-subdirs := $(addprefix $(LOCAL_PATH)/../../../,$(addsuffix /Android.mk, \
-           cocos2dx \
-           CocosDenshion/android \
-           lua/proj.android/jni \
-	))
-subdirs += $(LOCAL_PATH)/../../Classes/Android.mk $(LOCAL_PATH)/helloworld/Android.mk
+LOCAL_MODULE := game_shared
 
-include $(subdirs)
+LOCAL_MODULE_FILENAME := libgame
+
+LOCAL_SRC_FILES := helloworld/main.cpp
+
+LOCAL_WHOLE_STATIC_LIBRARIES := cocos_png_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos_jpeg_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos_xml2_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos2dx_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos_game_logic_static
+
+LOCAL_SHARED_LIBRARIES := lua_shared
+LOCAL_SHARED_LIBRARIES += cocosdenshion_shared
+
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,external/platform_external_libpng)
+$(call import-module,external/jpeg8d)
+$(call import-module,external/platform_external_libxml2)
+$(call import-module,HelloLua/Classes)
+$(call import-module,cocos2dx)
+$(call import-module,CocosDenshion/android)
+$(call import-module,lua/proj.android/jni)
