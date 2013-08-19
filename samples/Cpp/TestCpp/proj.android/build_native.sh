9a9d1f0e7601a88e358d1595bed560dd7cdf6395
folecr <folecr@gmail.com>
2013-08-18 22:48:29 -0700

    Verbose build logs : NDK_LOG=1 and V=1 in build_native.sh

--- samples/Cpp/TestCpp/proj.android/build_native.sh
+++ samples/Cpp/TestCpp/proj.android/build_native.sh
@@ -95,4 +95,5 @@ rm -f "$APP_ANDROID_ROOT"/assets/Images/test_1021x1024_a8.pvr.gz
 
 echo "Using prebuilt externals"
 "$NDK_ROOT"/ndk-build -C "$APP_ANDROID_ROOT" $* \
+    NDK_LOG=1 V=1 \
     "NDK_MODULE_PATH=${COCOS2DX_ROOT}:${COCOS2DX_ROOT}/cocos2dx/platform/third_party/android/prebuilt"
